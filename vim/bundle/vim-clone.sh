e614a3b62349797da7e9229875e71104d10281b0
Paul Munday <paul@paulmunday.net>
2016-12-02 11:57:38 -0800

    added pydocstring.vim

--- vim/bundle/vim-clone.sh
+++ vim/bundle/vim-clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for REPO in https://github.com/vim-utils/vim-troll-stopper.git https://github.com/davidhalter/jedi-vim.git  https://github.com/scrooloose/syntastic.git https://github.com/Valloric/MatchTagAlways.git https://github.com/majutsushi/tagbar.git https://github.com/tpope/vim-abolish.git https://github.com/tpope/vim-eunuch.git https://github.com/hynek/vim-python-pep8-indent.git https://github.com/plasticboy/vim-markdown.git https://github.com/bling/vim-airline https://github.com/vim-airline/vim-airline-themes.git https://github.com/leshill/vim-json.git https://github.com/eapache/rainbow_parentheses.vim.git https://github.com/tpope/vim-fireplace.git https://github.com/tpope/vim-unimpaired.git; do
+for REPO in https://github.com/vim-utils/vim-troll-stopper.git https://github.com/davidhalter/jedi-vim.git  https://github.com/scrooloose/syntastic.git https://github.com/Valloric/MatchTagAlways.git https://github.com/majutsushi/tagbar.git https://github.com/tpope/vim-abolish.git https://github.com/tpope/vim-eunuch.git https://github.com/hynek/vim-python-pep8-indent.git https://github.com/plasticboy/vim-markdown.git https://github.com/bling/vim-airline https://github.com/vim-airline/vim-airline-themes.git https://github.com/leshill/vim-json.git https://github.com/eapache/rainbow_parentheses.vim.git https://github.com/tpope/vim-fireplace.git https://github.com/tpope/vim-unimpaired.git git@github.com:heavenshell/vim-pydocstring.git; do
     git clone $REPO
 done
 cd jedi-vim
