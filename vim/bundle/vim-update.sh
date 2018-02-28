0e36ae35cc052850709ebf773a41ed34568fb8da
Paul Munday <paul.munday@coop.co.uk>
2018-02-28 10:14:37 +0000

    added vim-fugitive

--- vim/bundle/vim-update.sh
+++ vim/bundle/vim-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for REPO in jedi-vim  syntastic MatchTagAlways tagbar vim-abolish vim-eunuch vim-python-pep8-indent vim-markdown vim-flavored-markdown vim-airline vim-airline-themes vim-json vim-troll-stopper vim-fireplace rainbow_parentheses.vim vim-uimpaired vim-pydocstring vim-surround vim-clojure-static; do
+for REPO in jedi-vim  syntastic MatchTagAlways tagbar vim-abolish vim-eunuch vim-python-pep8-indent vim-markdown vim-flavored-markdown vim-airline vim-airline-themes vim-json vim-troll-stopper vim-fireplace rainbow_parentheses.vim vim-uimpaired vim-pydocstring vim-surround vim-clojure-static vim-fugitive; do
     cd $REPO
     git pull
     cd ../
-
 done
+
